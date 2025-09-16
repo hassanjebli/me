37391e39aef219cd0dfe7e690c1a13f3a46bc703
Hassan AIT JABLI <hassanjebli2002@gmail.com>
2025-09-16 20:25:04 +0000

    Prepare for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/me">
         <ParticlesBackground />
         <Navbar />
         <main className="min-h-screen pt-16">
